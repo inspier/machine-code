f4c28691fadda812d2280a873689a9fcf0f54eec
Göran Weinholt <goran@weinholt.se>
2017-01-06 13:45:53 +0100

    Add GNU Guile to the CI tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -ex
 
+SCHEME="${SCHEME-}"
+
 function cleanup {
   rm -f x86-demo.image x86-linux-demo.image
 }
 trap cleanup EXIT
 
-tests/arm-a64.sps
-tests/cpu12disasm.sps
-tests/string-table.sps
-tests/x86asm.sps
-tests/x86disasm.sps
+$SCHEME tests/arm-a64.sps
+$SCHEME tests/cpu12disasm.sps
+$SCHEME tests/string-table.sps
+$SCHEME tests/x86asm.sps
+$SCHEME tests/x86disasm.sps
 
-programs/x86-demo
-programs/fcdisasm -b 32 x86-demo.image | grep -5 rep
+$SCHEME programs/x86-demo
+$SCHEME programs/fcdisasm -b 32 x86-demo.image | grep -5 rep
 
-programs/x86-linux-demo
-programs/fcdisasm -b 32 x86-linux-demo.image
+$SCHEME programs/x86-linux-demo
+$SCHEME programs/fcdisasm -b 32 x86-linux-demo.image
 case $(uname -m) in
     x86_64|i386)
         chmod +x x86-linux-demo.image
