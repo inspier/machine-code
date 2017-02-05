6c024f168c222f2f55d40132a478c95c17fe826d
Göran Weinholt <goran@weinholt.se>
2017-02-05 19:15:06 +0100

    Move build-scheme to a separate repo, weinholt/scheme-ci

--- run-tests.sh
+++ run-tests.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -ex
 
-RUNSCHEME="${RUNSCHEME-}"
-
 function cleanup {
   rm -f x86-demo.image x86-linux-demo.image
 }
 trap cleanup EXIT
 
-$RUNSCHEME tests/cpu12disasm.sps
-$RUNSCHEME tests/string-table.sps
-$RUNSCHEME tests/x86asm.sps
-$RUNSCHEME tests/x86disasm.sps
+tests/cpu12disasm.sps
+tests/string-table.sps
+tests/x86asm.sps
+tests/x86disasm.sps
 
-$RUNSCHEME programs/x86-demo
-$RUNSCHEME programs/x86-linux-demo
+programs/x86-demo
+programs/x86-linux-demo
 
 case $(uname -m) in
     x86_64|i386)
@@ -23,9 +21,9 @@ case $(uname -m) in
         ;;
 esac
 
-if [ "x$SCHEME" != "xSagittarius" ]; then
+if [ "x$SCHEME" != "xSagittariusScheme" ]; then
     # Disabled in Sagittarius for now due to bad performance.
-    $RUNSCHEME programs/fcdisasm -b 32 x86-demo.image | grep -5 rep
-    $RUNSCHEME programs/fcdisasm -b 32 x86-linux-demo.image
-    $RUNSCHEME tests/arm-a64.sps
+    programs/fcdisasm -b 32 x86-demo.image | grep -5 rep
+    programs/fcdisasm -b 32 x86-linux-demo.image
+    tests/arm-a64.sps
 fi
